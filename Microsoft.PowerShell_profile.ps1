da1839892a507151cd18660911d9ae7e10852670
jonbrennecke <brennecke.jonathan@wsu.edu>
2014-04-11 11:03:27 -0700

    added aliases for x86 powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,8 @@ Set-Location C:\Users\brennecke.jonathan\Documents\GitHub\matlab-pipeline
 # set 'subl' as an alias to Sublime Text
 Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
 
+# alias for python
+Set-Alias python 'C:\Python27\python.exe'
+
+# alias for git
+Set-Alias git 'C:\Users\brennecke.jonathan\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin\git.exe'
