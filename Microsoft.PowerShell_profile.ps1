99e1f303c6a3487d4376482576d5845eaacb34de
Jon Brennecke <jpbrennecke@gmail.com>
2014-04-25 09:02:51 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,51 @@
-# start in the matlab folder
-Set-Location C:\Users\brennecke.jonathan\Documents\GitHub\matlab-pipeline
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Just a simple PowerShell script to get things started
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+echo " >> Welcome. Let's initializing the git repo"
+
+echo " >> checking if 'git' is installed"
+
+# if git isn't installed...
+if ( ! Get-Command "git" -ErrorAction SilentlyContinue ) {
+    
+    # location of git on TDT#4
+    if ( Test-Path 'C:\Users\brennecke.jonathan\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin\git.exe' ) {
+        Set-Alias git 'C:\Users\brennecke.jonathan\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin\git.exe'
+    }
+
+    # location of git on ULTRAROSS
+    elseif ( Test-Path 'C:\Users\wisorlab\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin\git.exe' ) {
+        
+    }
+
+    # finally, just give up and install git
+    else {
+
+        echo " >> Couldn't find 'git'. Downloading it from the internet..."
+
+        # download Posh-Git from psget.net and run installer
+        (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+
+        install-module posh-git 
+
+    }
+
+}
+
+# git ought to be installed by this point
+echo " >> Success!"
+
+# clone rempe's working directory
+git clone wisorlab:Epoch-Based-Processing
+
+# switch to rempe's branch
+git checkout mrempe
+
+
+
+# let's do a few other things...
+
 
 # set 'subl' as an alias to Sublime Text
 Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
@@ -7,5 +53,4 @@ Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
 # alias for python
 Set-Alias python 'C:\Python27\python.exe'
 
-# alias for git
-Set-Alias git 'C:\Users\brennecke.jonathan\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin\git.exe'
+# alias for emacs
